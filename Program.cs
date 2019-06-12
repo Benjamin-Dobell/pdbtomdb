b3bcaee3307679d937bf52b4a451dd8635a795c3
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2019-06-13 02:38:32 +1000

    Simple CLI tool to convert PDB to MDB

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Mono.Cecil;
+using Mono.Cecil.Mdb;
+using Mono.Cecil.Pdb;
+
+namespace PdbToMdb
+{
+	internal class Program
+	{
+		private static string Usage()
+		{
+			var prefix = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "" : "mono ";
+			return $"Usage: {prefix}PdbToMdb.exe <DLL path>";
+		}
+
+		public static int Main(string[] args)
+		{
+			if (args.Length != 1)
+			{
+				Console.Error.WriteLine(Usage());
+				return -1;
+			}
+
+			if (args[0].ToLower().EndsWith("help"))
+			{
+				Console.WriteLine(Usage());
+				return 0;
+			}
+
+			var assemblyFile = args[0];
+
+			var resolver = new DefaultAssemblyResolver();
+			resolver.AddSearchDirectory(Path.GetDirectoryName(assemblyFile));
+
+			var readerParams = new ReaderParameters {
+				AssemblyResolver = resolver,
+				ReadSymbols = true,
+				ReadWrite = true,
+				SymbolReaderProvider = new PdbReaderProvider(),
+			};
+
+			using (var assembly = AssemblyDefinition.ReadAssembly(assemblyFile, readerParams))
+			{
+				var writerParams = new WriterParameters {
+					SymbolWriterProvider = new MdbWriterProvider(),
+					WriteSymbols = true,
+				};
+
+				assembly.Write(writerParams);
+			}
+
+			Console.WriteLine("Done");
+			return 0;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# pdbtomdb
+
+A super simple CLI program to convert PDB to MDB (Mono). This project is trivial all the heavy lifting is done by [Cecil](https://cecil.pe/).
+
+# Usage:
+
+## Windows
+
+```
+PdbToMdb.exe <DLL path>
+```
+
+## UNIX
+
+```
+mono PdbToMdb.exe <DLL path>
+```
+
